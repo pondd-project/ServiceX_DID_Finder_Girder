d05c03ab63ac1b32b8d074c3561c7e15145685b9
Michael-D-Johnson <mdjohnson44@gmail.com>
2022-01-14 09:09:25 -0600

    updating did finder to pass data type

--- src/servicex_did_finder_girder.py
+++ src/servicex_did_finder_girder.py
@@ -33,37 +33,56 @@ import requests
 
 __log = logging.getLogger(__name__)
 
-
-async def girder_did_finder(did_name: str,
+async def girder_did_finder(did_name: str, 
                             info: Dict[str, Any]
                             ) -> AsyncGenerator[Dict[str, Any], None]:
     __log.info('DID Lookup request for collection {did_name}'.format(did_name=did_name),
-               extra={"requestId": info['request-id']})
+               extra = {"requestId": info['request-id']})
 
     root_url = "https://girder.hub.yt/api/v1/"
-    response = requests.get(root_url + "folder?parentType=collection&parentId={did_name}".format(
-            did_name=did_name))
+    did,did_type = did_name.split('#')
 
-    if len(response.json()) == 0:
-        # download collection if folders not found
-        __log.info('No folders found with parentId: {did_name}!'.format(did_name=did_name))
-        yield {
-            'file_path': root_url + 'collection/{coll_id}/download'.format(
-                coll_id=did_name),
-            'adler32': 0,
-            'file_size': 0,
-            'file_events': 0,
+    try:
+        if did_type == "collection":
+            # try downloading as collection
+            response = requests.get(root_url + "folder?parentType=collection&parentId={did}".format(
+                    did=did))
+
+            if len(response.json()) == 0:
+                # download collection if folders not found
+                __log.info('No folders found with parentId: {did_name}!'.format(did_name=did_name))
+                yield {
+                    'file_path': root_url + 'collection/{coll_id}/download'.format(
+                        coll_id=did_name),
+                    'adler32': 0,
+                    'file_size': 0,
+                    'file_events': 0,
+                }
+            else:
+                # if folders found in collection, iterate and yield download uri for each folder
+                for folder in response.json():
+                    yield {
+                        'file_path': root_url + 'folder/{folder_id}/download'.format(folder_id=folder['_id']),
+                        'adler32': 0,
+                        'file_size': 0,
+                        'file_events': 0,
+                    }
+        if did_type == "folder":
+            yield {
+                'file_path': root_url + 'folder/{folder_id}/download'.format(folder_id=did),
+                'adler32': 0,
+                'file_size': 0,
+                'file_events': 0,
             }
-
-    # if folders found, iterate and yield download uri for each folder
-    for folder in response.json():
-        yield {
-            'file_path': root_url + 'folder/{folder_id}/download'.format(folder_id=folder['_id']),
-            'adler32': 0,
-            'file_size': 0,
-            'file_events': 0,
+        if did_type == "item":
+            yield {
+                'file_path': root_url + 'item/{item_id}/download'.format(item_id=did),
+                'adler32': 0,
+                'file_size': 0,
+                'file_events': 0,
             }
-
+    except:
+        __log.info('Could not find dataset with id: {did_name}'.format(did_name=did_name))
 
 def run_girder_did_finder():
     log = logging.getLogger(__name__)
@@ -73,7 +92,6 @@ def run_girder_did_finder():
         start_did_finder('girder', girder_did_finder)
     finally:
         log.info('Done running girder DID finder')
-
 
 if __name__ == "__main__":
     run_girder_did_finder()
